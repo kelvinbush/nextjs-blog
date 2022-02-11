da3d258d7c15b4a02da10f762241a84c0f97166f
Kelvin Bush <kelybush@gmail.com>
2022-02-11 14:06:29 +0300

    Converting to typescript project

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,27 @@
 import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
 import Link from 'next/link'
 import Date from '../components/date'
+import { GetStaticProps } from 'next'
 
-import {getSortedPostsData} from '../lib/posts'
-import Layout, {siteTitle} from "../components/layout";
-import utilStyles from '../styles/utils.module.css'
-
-export async function getStaticProps() {
-    const allPostsData = getSortedPostsData()
-    return {
-        props: {
-            allPostsData
-        }
-    }
-}
-
-export default function Home({allPostsData}) {
+export default function Home({
+                                 allPostsData
+                             }: {
+    allPostsData: {
+        date: string
+        title: string
+        id: string
+    }[]
+}) {
     return (
         <Layout home>
             <Head>
                 <title>{siteTitle}</title>
             </Head>
             <section className={utilStyles.headingMd}>
-                <p>Kelvin Bush</p>
+                <p>[Your Self Introduction]</p>
                 <p>
                     (This is a sample website - you’ll be building a site like this in{' '}
                     <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
@@ -31,7 +30,7 @@ export default function Home({allPostsData}) {
             <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
                 <h2 className={utilStyles.headingLg}>Blog</h2>
                 <ul className={utilStyles.list}>
-                    {allPostsData.map(({id, date, title}) => (
+                    {allPostsData.map(({ id, date, title }) => (
                         <li className={utilStyles.listItem} key={id}>
                             <Link href={`/posts/${id}`}>
                                 <a>{title}</a>
@@ -46,4 +45,13 @@ export default function Home({allPostsData}) {
             </section>
         </Layout>
     )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+    const allPostsData = getSortedPostsData()
+    return {
+        props: {
+            allPostsData
+        }
+    }
 }
